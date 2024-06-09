70bce4823d90044640e5f89548d0e6a7197598d6
Alex <swisski@Alexs-PC>
2024-06-09 13:58:22 -0700

    lets goooo

---
+++ MoodMusic/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Song(models.Model):
+    title = models.CharField(max_length = 100)
+    artist = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return f"{self.title} by {self.artist}"
+    
+class UserSongMoods(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    song = models.ForeignKey(Song, on_delete = models.CASCADE)
+    moods = models.TextField(help_text = "Enter moods separated by commas")
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.song}"
+    
+    class Meta:
+        unique_together = ('user', 'song')
+
+
+def separate_moods(s):
+    return s.split(',')
