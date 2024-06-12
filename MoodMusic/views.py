6f4fd7dea3119da2dbb8ff89574b74e1a5e03b74
Alex Williams <agwilliams200@gmail.com>
2024-06-11 23:10:47 -0400

    Fucking around and finding out

--- MoodMusic/views.py
+++ MoodMusic/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import Http404, HttpResponse
+
+from MoodMusic.models import UserSongMoods, Song
+
+
+def test(request):
+    try:
+        p = UserSongMoods.objects.get(moods='Happy')
+    except UserSongMoods.DoesNotExist:
+        raise Http404("Usersongmood does not exist")
+    return HttpResponse(p.user.username)
